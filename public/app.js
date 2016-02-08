e826787a91eaa992fa208b4ff0ddeb08ca7ebcbc
Kraykin <kraykin@gmail.com>
2016-02-08 17:19:34 +0300

    Update add_to_cart function

--- public/app.js
+++ public/app.js
@@ -1,15 +1,15 @@
 function something()
 {
 	var x = window.localStorage.getItem('bbb');
-
 	x = x * 1 + 1;
-
 	window.localStorage.setItem('bbb', x);
-
 	alert('Hello ' + x + '!');	
 }
 
 function add_to_cart(id)
 {
-	alert('You addet pizza with id: ' + id);
+	var key = 'product_' + id;
+	var x = window.localStorage.getItem(key);
+	x = x * 1 + 1;
+	window.localStorage.setItem(key, x);
 }
