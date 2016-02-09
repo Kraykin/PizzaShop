f8d46096f59370c0783354ff0ccc6623c27136fc
Kraykin <kraykin@gmail.com>
2016-02-09 13:08:30 +0300

    Add cart_get_orders function

--- public/scripts.js
+++ public/scripts.js
@@ -37,6 +37,24 @@ function cart_get_number_of_items()
 	return cnt; // вернуть значение cnt
 }
 
+function cart_get_orders()
+{
+	var orders = '';
+	
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i);
+		var value = window.localStorage.getItem(key);
+		
+		if(key.indexOf('product_') == 0)
+		{
+			orders += key + '=' + value + ',';
+		}
+	}
+
+	return orders;
+}
+
 function cart_total()
 {
 	var total = 0;
